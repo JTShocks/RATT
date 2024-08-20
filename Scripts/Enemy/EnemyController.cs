755e7ea6f88857ba871b2463f2deb11b3904160d
JTShocks <jacobdreyer2002@gmail.com>
2024-08-20 10:10:16 -0400

    Started work on enemy behaviours

---
+++ Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class EnemyController : CharacterBody2D
+{
+
+    //The enemy controller should contain the calculations for the tracking and navigation
+
+
+    
+
+    
+}
