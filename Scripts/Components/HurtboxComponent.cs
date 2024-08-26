e999aa39542723a4968f38458501114edb8e603c
JTShocks <jacobdreyer2002@gmail.com>
2024-08-26 12:38:27 -0400

    Added debug color changer for Hurtboxes
    Can change color from a single node and have it update in the inspector

--- Scripts/Components/HurtboxComponent.cs
+++ Scripts/Components/HurtboxComponent.cs
@@ -5,6 +5,9 @@ using System;
 public partial class HurtboxComponent : StaticBody2D
 {
 
+
+  [ExportSubgroup("Debug")]
+    [Export] Color hurtboxColor;
 	[Export] public bool IsCriticalZone {get; private set;}
 	[Export] int DamageReduction = 0;
 
@@ -18,6 +21,16 @@ public partial class HurtboxComponent : StaticBody2D
       OnTakeDamage.Invoke(damage, this);
       GD.Print(Name + " got hit! \n Owner is: " + Owner.Name);
 
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        if(Engine.IsEditorHint())
+        {
+          CollisionShape2D hurtboxShape = GetNode<CollisionShape2D>("CollisionShape2D");
+          hurtboxShape.DebugColor = hurtboxColor;
+        }
     }
 
 
