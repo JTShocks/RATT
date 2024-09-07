d754cb7ab69b717b79ae30d8a4558d346f27fe32
JTShocks <jacobdreyer2002@gmail.com>
2024-09-07 17:08:12 -0400

    Added enemy prop
    Working on player skeleton

--- Scripts/StateMachine/EnemyStates/Attack.cs
+++ Scripts/StateMachine/EnemyStates/Attack.cs
@@ -5,6 +5,10 @@ public partial class Attack : EnemyState
 {
 
     //Add a resource for the specific attack behaviour for the attack state
+    public override void OnEnter(string previousStatePath)
+    {
+        base.OnEnter(previousStatePath);
+    }
 
 
 }
